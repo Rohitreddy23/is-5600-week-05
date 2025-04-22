42476fcbd953bef375d35dfe7d211f7c9b33df31
Rohitreddy23 <115164333+Rohitreddy23@users.noreply.github.com>
2025-04-22 03:21:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
 app.get('/orders', api.listOrders)
 app.post('/orders/', api.createOrder)
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id', api.deleteOrder)
 
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
+
 
